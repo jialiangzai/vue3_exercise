37ad6cbd25560841a1210f6624cc5bcb78f75161
zuishuainanhai <1705902650@qq.com>
2021-11-28 15:59:11 +0800

    回顾impl引用对象

--- src/views/computed.函数.vue
+++ src/views/computed.函数.vue
@@ -11,13 +11,18 @@
 <script>
 /** 使用步骤**
 1. 从vue框架中导入`computed` 函数
-2. 在setup函数中执行computed函数，并传入一个函数，在函数中定义计算公式
+2. 在setup函数中执行computed函数，并传入一个函数，在函数中定义计算公式 const 计算属性变量名 = computed(()=>{return 计算返回的结果})
 3. 把computed函数调用完的执行结果放到setup的return值对象中
  */
 import { ref, computed } from 'vue'
 export default {
   setup () {
     const list = ref([1, 2, 3, 4, 5])
+    // console.log(list)
+    // const aa = ref(0)
+    // console.log(aa)
+    // const bb = reactive({ nae: 5 })
+    // console.log(bb)
     const addData = () => {
       list.value.push(Math.floor(Math.random() * 100 + 1))
     }
